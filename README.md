ca3b6fd3e01d7afbbbcb8953e28440e058eefa1f
lyric <tiannianshou@gmail.com>
2016-07-23 18:01:54 +0800

    Add redis store

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# redis
-Redis token storage for OAuth 2.0
+Redis Token Store
+=================
+
+[![GoDoc](https://godoc.org/github.com/go-oauth2/redis?status.svg)](https://godoc.org/github.com/go-oauth2/redis)
+[![Go Report Card](https://goreportcard.com/badge/github.com/go-oauth2/redis)](https://goreportcard.com/report/github.com/go-oauth2/redis)
+
+Install
+-------
+
+``` bash
+$ go get -v github.com/go-oauth2/redis
+```
+
+Usage
+-----
+
+``` go
+package main
+
+import (
+	"github.com/go-oauth2/redis"
+	"gopkg.in/oauth2.v3/manage"
+)
+
+func main() {
+	manager := manage.NewDefaultManager()
+	// use redis store
+	manager.MustTokenStorage(redis.NewStore(&redis.Config{
+		Addr: "192.168.33.70:6379",
+	}))
+
+	// ...
+}
+```
+
+License
+-------
+
+```
+Copyright (c) 2016, OAuth 2.0
+All rights reserved.
+```
---
+++ redis_test.go
@@ -0,0 +1,74 @@
+package redis_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/go-oauth2/redis"
+	"gopkg.in/oauth2.v3/models"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestStore(t *testing.T) {
+	Convey("Test redis store", t, func() {
+		cfg := &redis.Config{
+			Addr: "192.168.33.70:6379",
+		}
+		store, err := redis.NewStore(cfg)
+		So(err, ShouldBeNil)
+
+		Convey("Test access token store", func() {
+			info := &models.Token{
+				ClientID:        "1",
+				UserID:          "1_1",
+				RedirectURI:     "http://localhost/",
+				Scope:           "all",
+				Access:          "1_1_1",
+				AccessCreateAt:  time.Now(),
+				AccessExpiresIn: time.Second * 5,
+			}
+			err := store.Create(info)
+			So(err, ShouldBeNil)
+
+			ainfo, err := store.GetByAccess(info.GetAccess())
+			So(err, ShouldBeNil)
+			So(ainfo.GetUserID(), ShouldEqual, info.GetUserID())
+
+			err = store.RemoveByAccess(info.GetAccess())
+			So(err, ShouldBeNil)
+
+			ainfo, err = store.GetByAccess(info.GetAccess())
+			So(err, ShouldBeNil)
+			So(ainfo, ShouldBeNil)
+		})
+
+		Convey("Test refresh token store", func() {
+			info := &models.Token{
+				ClientID:         "1",
+				UserID:           "1_2",
+				RedirectURI:      "http://localhost/",
+				Scope:            "all",
+				Access:           "1_2_1",
+				AccessCreateAt:   time.Now(),
+				AccessExpiresIn:  time.Second * 5,
+				Refresh:          "1_2_2",
+				RefreshCreateAt:  time.Now(),
+				RefreshExpiresIn: time.Minute * 1,
+			}
+			err := store.Create(info)
+			So(err, ShouldBeNil)
+
+			rinfo, err := store.GetByRefresh(info.GetRefresh())
+			So(err, ShouldBeNil)
+			So(rinfo.GetUserID(), ShouldEqual, info.GetUserID())
+
+			err = store.RemoveByRefresh(info.GetRefresh())
+			So(err, ShouldBeNil)
+
+			rinfo, err = store.GetByRefresh(info.GetRefresh())
+			So(err, ShouldBeNil)
+			So(rinfo, ShouldBeNil)
+		})
+	})
+}
